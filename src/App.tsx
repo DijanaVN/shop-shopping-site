ae32fbd916d6249de8308589bb055a40d1374973
DijanaVN <veljanovskadijana@yahoo.com>
2023-08-07 11:45:17 +0200

    inital commit + instaling chakra

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Text } from "@chakra-ui/react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Text>Ok let start</Text>;
+}
+
+export default App;
