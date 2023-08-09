62aee2b78fdbc7cc55f08eb96833aa15bf5370e1
DijanaVN <veljanovskadijana@yahoo.com>
2023-08-09 12:27:26 +0200

    Creating HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,44 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "../components/NavBar";
+import ProductGrid from "../components/ProductGrid";
+import ProductCategories from "../components/ProductCategories";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import CategoryUpdateForm from "../components/CategoryUpdateForm";
+
+function HomePage() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `'header' "nav" "main" "footer"`,
+        lg: `"header header" "nav nav"
+                  "aside main"
+                  "footer footer"`,
+      }}
+      gap="1"
+      color="red.700"
+      fontWeight="bold"
+    >
+      <GridItem pl="2" area={"header"}>
+        <Header />
+        <CategoryUpdateForm />
+      </GridItem>
+      <GridItem pl="2" area={"nav"}>
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem pl="2" area={"aside"}>
+          <ProductCategories />
+        </GridItem>
+      </Show>
+      <GridItem pl="2" area={"main"}>
+        <ProductGrid />
+      </GridItem>
+      <GridItem pl="2" area={"footer"}>
+        <Footer />
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default HomePage;
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import HomePage from "../pages/HomePage";
 
 createBrowserRouter([
   { path: "/", element: <HomePage /> },
