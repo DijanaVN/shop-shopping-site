02115694c22d302f0a8b25e237408afdd5ca0722
DijanaVN <veljanovskadijana@yahoo.com>
2023-08-10 21:04:49 +0200

    Create a delete category hook

---
+++ src/hooks/useDeleteCategory.ts
@@ -0,0 +1,33 @@
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import apiClientDetails from "./../services/api-client-details";
+import { Category, Product } from "./useProducts";
+
+const useDeleteCategory = () => {
+  const queryClient = useQueryClient();
+
+  const deleteCategoryMutation = useMutation(
+    (categoryId: number) => {
+      return apiClientDetails.delete<Boolean>(`categories/${categoryId}`);
+    },
+    {
+      // This callback will be triggered after a successful mutation
+      onSuccess: () => {
+        // Invalidate the 'categories' query in the cache to refresh the category list
+        queryClient.invalidateQueries(["categories"]);
+        console.log("Category deleted successfully");
+      },
+      onError: (error) => {
+        console.error("Error deleting category:", error);
+      },
+    }
+  );
+
+  return { deleteCategory: deleteCategoryMutation.mutate };
+};
+
+export default useDeleteCategory;
