da20c19063930d0cd1b7836e36e29c83f55f023c
DijanaVN <veljanovskadijana@yahoo.com>
2023-08-15 17:33:41 +0200

    Adding navigation in CategoryGridPage

--- src/pages/CategoryGridPage.tsx
+++ src/pages/CategoryGridPage.tsx
@@ -15,9 +15,12 @@ import {
 import { useSelectedCategoryContext } from "../StateManagement/SelectedCategoryContext";
 import usePoductsByCategory from "../hooks/useProductsByCategory";
 import image from "../images/milad-fakurian-HE1_K4_-QT8-unsplash.webp";
+import { useSelectedProductContext } from "../StateManagement/SelectedProductContext";
+import { Link } from "react-router-dom";
 
 const CategoryGridPage = () => {
   const { selectedCategory } = useSelectedCategoryContext();
+  const { onClick } = useSelectedProductContext();
 
   if (selectedCategory) {
     const { updatedProductsInCategory } =
@@ -33,34 +36,40 @@ const CategoryGridPage = () => {
         >
           {updatedProductsInCategory?.map((m) =>
             selectedCategory === m.category ? (
-              <Card key={m.id} bg={"primary.50"} maxW="sm" marginBottom={5}>
-                <CardBody>
-                  <Image src={m.image} alt={m.title} borderRadius="lg" />
-                  <Stack mt="6" spacing="3">
-                    <Heading size="md">{m.title}</Heading>
-                    <Text fontWeight={"bold"}>Category: {m.category}</Text>
-                    <Text>{m.title}</Text>
-                    <Text
-                      fontWeight={"extrabold"}
-                      color="primary.300"
-                      fontSize="2xl"
-                    >
-                      ${m.price}
-                    </Text>
-                  </Stack>
-                </CardBody>
-                <Divider />
-                <CardFooter>
-                  <ButtonGroup spacing="2">
-                    <Button variant="solid" colorScheme="blue">
-                      Buy now
-                    </Button>
-                    <Button variant="ghost" colorScheme="blue">
-                      Add to cart
-                    </Button>
-                  </ButtonGroup>
-                </CardFooter>
-              </Card>
+              <Link
+                to={`/product/${m.id}`}
+                onClick={() => onClick(m)}
+                key={m.id}
+              >
+                <Card key={m.id} bg={"primary.50"} maxW="sm" marginBottom={5}>
+                  <CardBody>
+                    <Image src={m.image} alt={m.title} borderRadius="lg" />
+                    <Stack mt="6" spacing="3">
+                      <Heading size="md">{m.title}</Heading>
+                      <Text fontWeight={"bold"}>Category: {m.category}</Text>
+                      <Text>{m.title}</Text>
+                      <Text
+                        fontWeight={"extrabold"}
+                        color="primary.300"
+                        fontSize="2xl"
+                      >
+                        ${m.price}
+                      </Text>
+                    </Stack>
+                  </CardBody>
+                  <Divider />
+                  <CardFooter>
+                    <ButtonGroup spacing="2">
+                      <Button variant="solid" colorScheme="blue">
+                        Buy now
+                      </Button>
+                      <Button variant="ghost" colorScheme="blue">
+                        Add to cart
+                      </Button>
+                    </ButtonGroup>
+                  </CardFooter>
+                </Card>
+              </Link>
             ) : (
               ""
             )
