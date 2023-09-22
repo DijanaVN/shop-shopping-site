14c49ac2f907d1b17180c7e166bbf90314451c2f
DijanaVN <102368690+DijanaVN@users.noreply.github.com>
2023-09-22 13:13:38 +0200

    add serverless function

---
+++ server/api/handlerFunction.js
@@ -0,0 +1,31 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY || "", {
+  apiVersion: "2023-08-16",
+});
+
+export const handlerFunction = async (event, context) => {
+  try {
+    const { body } = event;
+    const lineItems = JSON.parse(body).lineItems;
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: lineItems,
+      mode: "payment",
+      payment_method_types: ["card"],
+      success_url: `${process.env.CLIENT_URL}/success`,
+      cancel_url: `${process.env.CLIENT_URL}/cancel`,
+    });
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify(session),
+    };
+  } catch (error) {
+    console.error("Error creating checkout session:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+    };
+  }
+};
