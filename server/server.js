13c8b1b21edd613ccdf919aa918f7676c2f33edc
DijanaVN <102368690+DijanaVN@users.noreply.github.com>
2023-09-22 14:21:07 +0200

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,46 +1,36 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-const express_1 = __importDefault(require("express"));
-const stripe_1 = __importDefault(require("stripe"));
-const cors_1 = __importDefault(require("cors"));
-dotenv_1.default.config();
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use((0, cors_1.default)());
-app.use(express_1.default.urlencoded({ extended: true }));
-const stripe = new stripe_1.default(process.env.STRIPE_PRIVATE_KEY || "", {
-    apiVersion: "2023-08-16",
+import dotenv from "dotenv";
+import express from "express";
+import Stripe from "stripe";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY || "", {
+  apiVersion: "2023-08-16",
 });
-app.post("/checkout", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const session = yield stripe.checkout.sessions.create({
-            line_items: req.body.lineItems,
-            mode: "payment",
-            payment_method_types: ["card"],
-            success_url: `${process.env.CLIENT_URL}/success`,
-            cancel_url: `${process.env.CLIENT_URL}/cancel`, //
-        });
-        return res.status(201).json(session);
-    }
-    catch (error) {
-        console.error("Error creating checkout session:", error);
-        return res.status(500).json(error);
-    }
-}));
+
+app.post("/checkout", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: req.body.lineItems,
+      mode: "payment",
+      payment_method_types: ["card"],
+      success_url: `${process.env.CLIENT_URL}/success`,
+      cancel_url: `${process.env.CLIENT_URL}/cancel`,
+    });
+    return res.status(201).json(session);
+  } catch (error) {
+    console.error("Error creating checkout session:", error);
+    return res.status(500).json(error);
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
