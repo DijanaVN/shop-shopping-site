91ee4c50da6f18c562f5188a05df342bcc8945c4
DijanaVN <veljanovskadijana@yahoo.com>
2023-10-24 17:28:35 +0200

    refactor server.js

--- server/server.js
+++ server/server.js
@@ -8,31 +8,9 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow these HTTP methods
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  ); // Allow these headers
-  next();
-});
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      defaultSrc: ["'self'"],
-      fontSrc: ["'self'", "<URL for fonts>"], // Add the URL for fonts here
-      styleSrc: ["'self'", "'unsafe-inline'"],
-      scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-      imgSrc: ["'self'", "data:"],
-      connectSrc: ["'self'", "<API endpoint URL>"], // Add the URL for API endpoints here
-      // Add other necessary directives for your applica
-    },
-  })
-);
 
 const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY || "", {
   apiVersion: "2023-10-16",
